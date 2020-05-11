ffc5673e00659cef3b8030bc5809d4f90d5d9f0f
Yannic Kalka <y.kalka@outlook.com>
2020-05-11 11:47:32 +0000

    fix type errors

--- libs/jwt/src/token-resolver.ts
+++ libs/jwt/src/token-resolver.ts
@@ -1,19 +1,19 @@
-import { Request } from 'express';
+import type { Request } from 'express';
 
 export function getTokenByBearerHeader() {
-  return (req: Request) => {
+  return (req: Request<any>): string => {
     return req.headers.authorization?.replace('Bearer ', '');
   };
 }
 
 export function getTokenByCookie(cookieName: string) {
-  return (req: Request) => {
+  return (req: Request<any>): string => {
 
     // check that cookies is definied
     if (!req.cookies) {
       throw Error('Cookies property not found. Please ensure that "cookie-parser" is installed');
     }
 
-    return req.cookies[cookieName];
+    return req.cookies[cookieName] as string;
   };
 }
